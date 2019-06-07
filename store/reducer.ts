8abcc2486dcf86e3a296c861619c7dcee2a6986c
Rebecca <rebecca_517@hotmail.com>
2019-06-07 12:04:46 -0400

    Create new Redux interface Event & CalendarState

---
+++ store/reducer.ts
@@ -0,0 +1,12 @@
+import {
+    CalendarState   
+} from './types';
+
+const initialState : CalendarState = {
+    events: []   
+};
+
+export function reducer(state = initialState, action) : CalendarState {
+    
+};
+
---
+++ store/types.ts
@@ -0,0 +1,12 @@
+export interface Event {
+    date: string,
+    startTime: string,
+    endTime: string,
+    location: string,
+    notes: string,
+    username: string    //not alterable
+};
+
+export interface CalendarState {
+    events: Event[]
+};
